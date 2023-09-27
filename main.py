813ee5214aabb516937621b3d0dd6ce65f949b5c
KM <kevin.morin79@gmail.com>
2023-09-27 22:13:22 +0200

    u

--- main.py
+++ main.py
@@ -21,18 +21,14 @@ def send_to_telegram(message):
 
 
 if __name__ == '__main__':
-    
     EXCEPTED_PLEX_RETURNED_MESSAGE = b"<html><head><script>window.location = window.location.href.match(/(^.+\\/)[^\\/]*$/)[1] + 'web/index.html';</script><title>Unauthorized</title></head><body><h1>401 Unauthorized</h1></body></html>"
     PLEX_SERVER = 'tremoille.ddns.net:29144'
 
-    # Call Plex Server
-    cmd = ['curl', PLEX_SERVER]
-    plexReturnedOutput = subprocess.check_output(cmd)
-    if EXCEPTED_PLEX_RETURNED_MESSAGE == plexReturnedOutput:
-        print('ok')
-        send_to_telegram("Call Plex Server : ok")
-        exit(0)
-    else:
-        print("ko")
-        send_to_telegram("Call Plex Server : ko")
-        exit(1)
+    cmd = ['curl', '--connect-timeout', '15', PLEX_SERVER]
+    if subprocess.call(cmd) == 0:
+        plexReturnedOutput = subprocess.check_output(cmd)
+        if EXCEPTED_PLEX_RETURNED_MESSAGE == plexReturnedOutput:
+            send_to_telegram("Call Plex Server : ok")
+            exit(0)
+    send_to_telegram("Call Plex Server : ko")
+    exit(1)
