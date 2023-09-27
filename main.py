85a50cd15cc9451c0bf34479f801dec21e6b4602
Your Name <you@example.com>
2023-09-27 12:11:39 +0200

    u

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+"""
+Project name : Korbel
+Description : Call Plex Server, send message to Telegram if KO
+Creation date : 27/09/2023
+Author : KMO 
+"""
+
 import subprocess
-
-EXCEPTED_PLEX_RETURNED_MESSAGE = b"<html><head><script>window.location = window.location.href.match(/(^.+\\/)[^\\/]*$/)[1] + 'web/index.html';</script><title>Unauthorized</title></head><body><h1>401 Unauthorized</h1></body></html>"
-
-# Call Plex Server
-cmd = ['curl', 'tremoille.ddns.net:29144']
-plexReturnedOutput = subprocess.check_output(cmd)
-if EXCEPTED_PLEX_RETURNED_MESSAGE == plexReturnedOutput:
-    print('ok')
-    exit(0)
-else:
-    print("ko")
-    exit(1)
+import requests
 
 
+def send_to_telegram(message):
+    apiToken = '1223180354:AAHWv4JPS7bpDJmccIvoqOhNrmtsWVz8fkE'
+    chatID = '531244299'
+    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+    try:
+        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+        print(response.text)
+    except Exception as e:
+        print(e)
 
 
+if __name__ == '__main__':
+    
+    EXCEPTED_PLEX_RETURNED_MESSAGE = b"<html><head><script>window.location = window.location.href.match(/(^.+\\/)[^\\/]*$/)[1] + 'web/index.html';</script><title>Unauthorized</title></head><body><h1>401 Unauthorized</h1></body></html>"
+    PLEX_SERVER = 'tremoille.ddns.net:29144'
+
+    # Call Plex Server
+    cmd = ['curl', PLEX_SERVER]
+    plexReturnedOutput = subprocess.check_output(cmd)
+    if EXCEPTED_PLEX_RETURNED_MESSAGE == plexReturnedOutput:
+        print('ok')
+        send_to_telegram("Call Plex Server : ok")
+        exit(0)
+    else:
+        print("ko")
+        send_to_telegram("Call Plex Server : ok")
+        exit(1)
