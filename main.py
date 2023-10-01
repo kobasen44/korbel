575eec7ebaa3cd6cb3ee1b7be1b6a1f2bafde1aa
kobasen44 <kevin.morin79@gmail.com>
2023-10-01 17:19:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ Author : KMO
 
 import subprocess
 import requests
+import os
 
 
 def send_to_telegram(message):
-    API_TOKEN = '1223180354:AAHWv4JPS7bpDJmccIvoqOhNrmtsWVz8fkE'
-    CHAT_ID = '531244299'
+    # API_TOKEN = '1223180354:AAHWv4JPS7bpDJmccIvoqOhNrmtsWVz8fkE'
+    API_TOKEN = os.environ['API_TOKEN']
+    # CHAT_ID = '531244299'
+    CHAT_ID = os.environ['CHAT_ID']
     API_URL = f'https://api.telegram.org/bot{API_TOKEN}/sendMessage'
     try:
         response = requests.post(API_URL, json={'chat_id': CHAT_ID, 'text': message})
@@ -22,7 +25,8 @@ def send_to_telegram(message):
 
 if __name__ == '__main__':
     EXCEPTED_PLEX_RETURNED_MESSAGE = b"<html><head><script>window.location = window.location.href.match(/(^.+\\/)[^\\/]*$/)[1] + 'web/index.html';</script><title>Unauthorized</title></head><body><h1>401 Unauthorized</h1></body></html>"
-    PLEX_SERVER = 'tremoille.ddns.net:29144'
+    # PLEX_SERVER = 'tremoille.ddns.net:29144'
+    PLEX_SERVER = os.environ['PLEX_SERVER']
 
     cmd = ['curl', '--connect-timeout', '15', PLEX_SERVER]
     if subprocess.call(cmd) == 0:
