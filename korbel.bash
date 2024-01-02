b1fd3c84e04e02239ca60da5b65ef3766b4566f7
Kevin Morin <kevin.morin@externe.maif.fr>
2024-01-02 21:59:22 +0100

    u

--- korbel.bash
+++ korbel.bash
@@ -3,7 +3,13 @@
 # Description : Call Plex Server, send message to Telegram if KO
 # Creation date : 27/09/2023
 # Author : KMO
+# Usage : ./korbel.bash
 
+################ CONSTANTS #################
+ATTEMPTS_NUMBER=4
+EXPECTED_PLEX_RETURNED_MESSAGE="<html><head><script>window.location = window.location.href.match(/(^.+\\/)[^\\/]*$/)[1] + 'web/index.html';</script><title>Unauthorized</title></head><body><h1>401 Unauthorized</h1></body></html>"
+
+################## FUNCTION ################
 send_to_telegram(){
   local API_URL="https://api.telegram.org/bot$API_TOKEN/sendMessage"
   local message="$1"  
@@ -11,14 +17,16 @@ send_to_telegram(){
   curl -s -X POST -H "Content-Type: application/json" -d "$data" "$API_URL"
 }
 
-EXPECTED_PLEX_RETURNED_MESSAGE="<html><head><script>window.location = window.location.href.match(/(^.+\\/)[^\\/]*$/)[1] + 'web/index.html';</script><title>Unauthorized</title></head><body><h1>401 Unauthorized</h1></body></html>"
-
-if cmd_output=$(curl --connect-timeout 15 "$PLEX_SERVER" 2>/dev/null); then
-  if [ "$EXPECTED_PLEX_RETURNED_MESSAGE" = "$cmd_output" ]; then
-    #send_to_telegram "Call Plex Server: ok"
-    exit 0
+################## MAIN ####################
+for ((i=1; i<=$ATTEMPTS_NUMBER; i++));
+do
+  if output=$(curl --connect-timeout 20 "$PLEX_SERVER" 2>/dev/null); then
+    if [ "$EXPECTED_PLEX_RETURNED_MESSAGE" = "$output" ]; then
+      #send_to_telegram "Call Plex Server: ok"
+      exit 0
+    fi
   fi
-fi
+done
 
 send_to_telegram "Call Plex Server: ko"
 exit 1
